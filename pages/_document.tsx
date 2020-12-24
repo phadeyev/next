3a7db376bd338773ae23dc5c088838f7230e2bcc
Dmitrii.Fadeev <dmitrii.fadeev@sebn.com>
2020-12-24 15:44:53 +0200

    first commit

---
+++ pages/_document.tsx
@@ -0,0 +1,21 @@
+import Document, { Head, Html, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps};
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+        <body>
+         <Main />
+         <NextScript />
+        </body>
+        </Head>
+      </Html>
+    )
+  }
+}
