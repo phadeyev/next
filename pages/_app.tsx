e5e61982d075b8a8a89779ec1c4567b37d686266
Dmitrii.Fadeev <dmitrii.fadeev@sebn.com>
2020-12-24 16:19:09 +0200

    day1

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,22 @@
-import App from 'next/app';
+import { useEffect } from 'react';
 import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { ThemeProvider } from '@material-ui/core/styles';
 
-export default ({ Component, pageProps }) => (
-  return <Component {..pageProps} />
-)
+import { themeDark, themeLight } from 'lib/theme';
+
+export default ({ Component, pageProps }) => {
+
+  useEffect(() => {
+      // Remove the server-side injected CSS
+      const jssStyles = document.querySelector('#jss-server-side');
+      if (jssStyles && jssStyles.parentNode) {
+        jssStyles.parentNode.removeChild(jssStyles);
+      }
+  }, []);
+  return (
+    <ThemeProvider theme={false ? themeDark : themeLight}>
+      <Component {...pageProps} /> )
+    </ThemeProvider>
+  )
+}
